a569f27aea4c6d4b49f40a2296af75f05bb0c935
WaterGenie35 <thirdwatergenie@gmail.com>
2025-07-29 16:03:27 +0700

    Minor syntax improvement

--- src/project_euler/problems_1_to_50/problem_1_multiples_of_3_or_5.py
+++ src/project_euler/problems_1_to_50/problem_1_multiples_of_3_or_5.py
@@ -5,7 +5,7 @@ def solution() -> None:
 
     Find the sum of all the multiples of 3 or 5 below 1_000.
     """
-    print(sum_of_multiples_of_3_or_5(1_000))
+    print(sum_of_multiples_of_3_or_5(lt_bound=1_000))
 
 
 def sum_of_multiples_of_3_or_5(lt_bound: int) -> int:
