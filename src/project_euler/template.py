d38dc0ca9af0cdc68e481d80c44c8f5e4e16644c
WaterGenie35 <thirdwatergenie@gmail.com>
2025-07-29 15:39:59 +0700

    Add boilerplate code for project euler problems

---
+++ src/project_euler/template.py
@@ -0,0 +1,16 @@
+def solution() -> None:
+    """
+    Solution to Project Euler problem x
+    https://projecteuler.net/problem=x
+
+    promopt
+    """
+    print(func())
+
+
+def func() -> int:
+    return 0
+
+
+def test_solution() -> None:
+    assert False
