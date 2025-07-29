b7da67129087ca910ed220db5ab48f5bd206b8c7
WaterGenie35 <thirdwatergenie@gmail.com>
2025-07-29 14:55:16 +0700

    Add solution to project euler problem 1

---
+++ src/project_euler/problems_1_to_50/problem_1_multiples_of_3_or_5.py
@@ -0,0 +1,17 @@
+def solution() -> None:
+    """
+    Solution to Project Euler problem 1
+    https://projecteuler.net/problem=1
+
+    Find the sum of all the multiples of 3 or 5 below 1_000.
+    """
+    print(sum_of_multiples_of_3_or_5(1_000))
+
+
+def sum_of_multiples_of_3_or_5(lt_bound: int) -> int:
+    return sum(n for n in range(1, lt_bound) if n % 3 == 0 or n % 5 == 0)
+
+
+def test_solution() -> None:
+    assert sum_of_multiples_of_3_or_5(10) == 23
+    assert sum_of_multiples_of_3_or_5(1_000) == 233_168
