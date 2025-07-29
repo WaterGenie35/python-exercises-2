76d8252321d69a5e727e763e26194b6d7c645652
WaterGenie35 <thirdwatergenie@gmail.com>
2025-07-29 15:23:37 +0700

    Add boilerplate code for leetcode problems

---
+++ src/leetcode/template.py
@@ -0,0 +1,16 @@
+class Solution:
+    """
+    Solution to LeetCode problem x, NeetCode y - category
+    leetcode_link
+    neetcode_link
+
+    prompt
+    """
+
+    def func(self) -> None:
+        pass
+
+
+def test_solution() -> None:
+    solution = Solution()
+    assert False
