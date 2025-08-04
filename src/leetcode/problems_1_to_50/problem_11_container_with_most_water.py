1470295b35fea422e1017a43c603519e58b8095d
WaterGenie35 <thirdwatergenie@gmail.com>
2025-08-04 17:39:44 +0700

    Add solution to leetcode problem 11 (neetcode 150 - two pointers)

---
+++ src/leetcode/problems_1_to_50/problem_11_container_with_most_water.py
@@ -0,0 +1,37 @@
+from utils.typing import Nullable
+
+
+class Solution:
+    """
+    Solution to LeetCode problem 11, NeetCode 150 - two pointers
+    https://leetcode.com/problems/container-with-most-water/
+    https://neetcode.io/problems/max-water-container
+
+    Given an interger array of heights of each bar, return the maximum amount of
+    area defined by the rectangle whose height is the minimum of the 2 chosen bars
+    and whose width is the difference between the indices.
+    """
+
+    def max_area(self, heights: list[int]) -> int:
+        area = 0
+        head = 0
+        tail = len(heights) - 1
+        while head < tail:
+            height_head = heights[head]
+            height_tail = heights[tail]
+            height = min(height_head, height_tail)
+            width = tail - head
+            new_area = width * height
+            area = max(area, new_area)
+
+            if height_head <= height_tail:
+                head += 1
+            else:
+                tail -= 1
+        return area
+
+
+def test_solution() -> None:
+    solution = Solution()
+    assert solution.max_area([2, 2, 2]) == 4
+    assert solution.max_area([1, 7, 2, 5, 4, 7, 3, 6]) == 36
